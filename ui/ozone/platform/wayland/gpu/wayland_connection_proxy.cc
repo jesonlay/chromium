46600654d50e780df502ff3b9bc0a426dca54b9a
Maksim Sisov <msisov@igalia.com>
2018-08-06 12:39:04 +0300

    fixup! [DoNotCarryForward] [ozone/Wayland] Implement Wayland dmabuf approach.
    Fix content_unittests, which run in a single thread context.

--- ui/ozone/platform/wayland/gpu/wayland_connection_proxy.cc
+++ ui/ozone/platform/wayland/gpu/wayland_connection_proxy.cc
@@ -12,8 +12,10 @@
 namespace ui {
 
 WaylandConnectionProxy::WaylandConnectionProxy(WaylandConnection* connection)
-    : connection_(connection),
-      ui_runner_(base::ThreadTaskRunnerHandle::Get()) {}
+    : connection_(connection) {
+  if (!connection_)
+    ui_runner_ = base::ThreadTaskRunnerHandle::Get();
+}
 
 WaylandConnectionProxy::~WaylandConnectionProxy() = default;
 
